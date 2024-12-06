eb51ec758763e094f50f6da904981f06b11a763a
KubaK1P <1p22kus@gmail.com>
2024-12-06 11:17:00 +0100

    refactor(config): moved magic numbers into config
    Refactored some magic constants into JSON-encoded configuration values.
    
    Co-authored-by: 1p22geo <1p22geodecki@gmail.com>

--- board.py
+++ board.py
@@ -1,4 +1,6 @@
 from math import floor
+import os
+
 
 class Board:
     def __init__(self, width: int, height: int, char: str) -> None:
@@ -7,25 +9,33 @@ class Board:
         self.char = char
         self.board = [[char for i in range(width)] for j in range(height)]
 
+    def clear_board(self):
+        if os.name == "nt":
+            os.system("cls")
+        else:
+            os.system("clear")
+
     def show_board(self):
+        self.clear_board()
         for i in self.board:
             for j in i:
                 print(j, end="\t")
             print("")
-        
+
         for i in range(1, self.width + 1):
             print(i, end="\t")
-        
+
         print("")
 
     def change_cell_char(self, x: int, y: int, char: str):
         if x < 0 or y < 0 or len(char) != 1 or x > self.width or y > self.height:
             raise IndexError(f"Values are out of bounds {x} {y}")
-        
-        self.board[self.height - floor(y) - 1][self.width - floor(x) - 1] = char
+
+        self.board[self.height -
+                   floor(y) - 1][self.width - floor(x) - 1] = char
 
     def get_cell_char(self, x: int, y: int) -> str | None:
         if x < 0 or y < 0 or x > self.width or y > self.height:
             raise IndexError(f"Values are out of bounds {x} {y}")
-        
+
         return self.board[self.height - floor(y) - 1][self.width - floor(x) - 1]
--- main.py
+++ main.py
@@ -1,66 +1,49 @@
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-from kevin import Kevin
 from time import sleep
 from board import Board
-from os import system, name
+from items.item import Item
+from items.kevin import Kevin
+from config import config
 
-x = [] # arrays to plot
+x = []  # arrays to plot
 y = []
 
-board = Board(10, 10, ".") # the display object
-kevins = [Kevin(4, 8), Kevin(6, 6)] # list of moving objects (kevins)
+MAX_ITERATIONS = config.get("config.loop.iterations")
+
+board = Board(10, 10, ".")  # the display object
+# list of moving objects (kevins)
+kevins: list[Item] = [Kevin(4, 8), Kevin(6, 6)]
 
 
-# initialize kevins with some acceleration and velocity
-for kevin in kevins:
-    kevin.a = np.array([0., -0.2])
-    kevin.v = np.array([0., 0.])
+# main code loop
+iteration = config.get("config.loop.start")
 
-
-# main code loop 
-for iteration in range(200):
-    for kevin in kevins:   
+while True:
+    iteration += 1
+    if MAX_ITERATIONS >= 0 and iteration > MAX_ITERATIONS:
+        break
+    for kevin in kevins:
         try:
             # clear the position after kevin
             board.change_cell_char(kevin.pos[0], kevin.pos[1], ".")
-
-            # update kevins' velocity and position 
-            kevin.v += kevin.a
-            kevin.pos += kevin.v
-        
-            if iteration == 100:
-                # add some variation in the 100th iteration
-                kevin.v += np.array([0., 2.])
-
-            if kevin.pos[1] < 0:
-                # make kevins bouncy
-                kevin.v *= -0.93
-                kevin.pos[1] = 0
-
             # draw kevin
+            kevin.propagate(iteration)
             board.change_cell_char(kevin.pos[0], kevin.pos[1], "K")
-            
+
         # handlen't errors
         except IndexError as e:
             print(f"Index error: {e}")
 
         except Exception as e:
             raise e
-    
+
     board.show_board()
-    sleep(1./24.)
-    
-    # clear thy screen
-    if name == "nt":
-        system("cls")
-    else:
-        system("clear")
-    
+    sleep(1./float(config.get("config.loop.fps")))
+
     x.append(iteration)
     y.append(kevin.pos[1])
-    
+
 
 plt.plot(np.array(x), np.array(y))
 plt.show()
